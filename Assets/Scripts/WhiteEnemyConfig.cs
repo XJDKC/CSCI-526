b91a0a1a95427c7ea900a105d2a796bbf24c6b25
msyysm1211 <mashengyuan1997@gmail.com>
2022-09-23 13:56:35 -0700

    generate stars

--- Assets/Scripts/WhiteEnemyConfig.cs
+++ Assets/Scripts/WhiteEnemyConfig.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class WhiteEnemyConfig : MonoBehaviour
 {
+    public Object startPrefab = null;
+
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.CompareTag("Player"))
         {
-            Destroy(GameObject.Find(this.transform.parent.name));
+            GameObject parent = transform.parent.gameObject;
+            GameObject blackEnemy = parent.transform.GetChild(1).gameObject;
+
+            if (startPrefab)
+            {
+                Quaternion rotation = new Quaternion();
+                rotation.eulerAngles = new Vector3(0.0f, 0.0f, Random.Range(0, 180.0f));
+                Instantiate(startPrefab, blackEnemy.transform.position, rotation);
+            }
+
+            Destroy(transform.parent.gameObject);
         }
     }
 }
