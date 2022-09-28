f75ab74c2be8278318d915c26d056660e306bd9b
JohnsonL-G8 <liusiyug8@gmail.com>
2022-09-27 19:03:27 -0700

    fix star collect bug & merge FadeOut.cs script

--- Assets/Scripts/Star.cs
+++ Assets/Scripts/Star.cs
@@ -1,9 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Star : MonoBehaviour
 {
+
+    [SerializeField]
+    public float delaySecond = 5F;
+
+    [SerializeField]
+    private float fadeSpeed = 0;
+    private SpriteRenderer spriteRenderer = null;
+
+
     //Collecting stars and updaing points
     void OnTriggerEnter2D(Collider2D collider)
     {
@@ -11,8 +21,32 @@ public class Star : MonoBehaviour
             collider.GetType().ToString() == "UnityEngine.CapsuleCollider2D")
         {
             StarUI.CurrentStarQuantity += 1;
-            Debug.Log(StarUI.CurrentStarQuantity);
+            //Debug.Log(StarUI.CurrentStarQuantity);
             Destroy(gameObject);
         }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (TryGetComponent(out SpriteRenderer spriteRenderer))
+            this.spriteRenderer = spriteRenderer;
+        fadeSpeed = this.spriteRenderer.color.a * Time.fixedDeltaTime / delaySecond;
+        StartCoroutine(DestroyNow());
+    }
+
+    //Destory after delaySecond time
+    private IEnumerator DestroyNow()
+    {
+        yield return new WaitForSeconds(delaySecond);
+        Destroy(gameObject);
+    }
+
+    private void FixedUpdate()
+    {
+        float alpha = spriteRenderer.color.a - fadeSpeed;
+        spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.r, spriteRenderer.color.r, alpha);
+        if (alpha <= 0)
+            Destroy(gameObject);
     }
 }
