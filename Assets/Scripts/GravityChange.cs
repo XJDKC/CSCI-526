f9df0c798a1c3d9046f81d282ce598cd75c27bbd
Haochenyang <haochenyang@umail.ucsb.edu>
2022-09-28 20:04:28 -0700

    improve player's gravity momentum and add some force to players with low velocity when going through gates

--- Assets/Scripts/GravityChange.cs
+++ Assets/Scripts/GravityChange.cs
@@ -9,6 +9,7 @@ public class GravityChange : MonoBehaviour
 {
     private Vector3 _upVector = Vector3.up;
     private Dictionary<Collider2D, float> _colliderEnterSide = new Dictionary<Collider2D, float>();
+    private Dictionary<Collider2D, float> _colliderEnterMid = new Dictionary<Collider2D, float>();
 
     private void Start()
     {
@@ -18,40 +19,47 @@ public class GravityChange : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        //ignore player with box collider
+        // ignore player with box collider
         if (collider.gameObject.tag == "Player" && collider.GetType() == typeof(BoxCollider2D)) return;
 
-
-        Vector3 center = transform.position;
-        Vector3 colliderCenter = collider.bounds.center;
-
-        // Vector from door to character: character's center - door's center position
-        Vector3 doorToObj = (center - colliderCenter).normalized;
-        float enterSide = Vector3.Cross(_upVector, doorToObj).z;
-        _colliderEnterSide[collider] = enterSide;
+        // if player do not have much velocity when cross the horizontal door, add a force
+        Rigidbody2D player = collider.GetComponent<Rigidbody2D>();
+        if (Math.Abs(player.velocity.y) < 2)
+        {
+            if (player.velocity.y > 0)  player.AddForce(new Vector2(0, 300));
+            if (player.velocity.y < 0)  player.AddForce(new Vector2(0, -300));
+        }
     }
 
-    private void OnTriggerExit2D(Collider2D collider)
-    {
-        //ignore player with box collider
+    private void OnTriggerStay2D(Collider2D collider) {
+        // ignore player with box collider
         if (collider.gameObject.tag == "Player" && collider.GetType() == typeof(BoxCollider2D)) return;
 
+        // find door's center and player's center
         Vector3 center = transform.position;
         Vector3 colliderCenter = collider.bounds.center;
 
-        // Vector from door to character: character's center - door's center position
+        // vector from door to character: character's center - door's center position
         Vector3 doorToObj = (center - colliderCenter).normalized;
-        float leaveSide = Vector3.Cross(_upVector, doorToObj).z;
+        float side = Vector3.Cross(_upVector, doorToObj).z;
 
-        if (_colliderEnterSide.ContainsKey(collider))
-        {
-            float enterSide = _colliderEnterSide[collider];
+        // compare the previous side with current side vector cross product
+        if (_colliderEnterMid.ContainsKey(collider)){
+            float prevSide = _colliderEnterMid[collider];
 
-            if (enterSide * leaveSide < 0.0)
+            if (side * prevSide < 0.0)
             {
                 collider.gameObject.GetComponent<Rigidbody2D>().gravityScale *= -1;
                 collider.gameObject.GetComponent<PlayerController>().Reverse();
             }
         }
+
+        _colliderEnterMid[collider] = side;
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        // ignore player with box collider
+        if (collider.gameObject.tag == "Player" && collider.GetType() == typeof(BoxCollider2D)) return;
     }
 }
