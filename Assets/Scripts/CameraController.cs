205a85c5f7524db0ba3aeee89cd3d5dfd585511d
icecoffee98 <yixuanyu98@gmail.com>
2022-09-30 01:10:13 -0700

    lerped position/size, revised naming, restructured code

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,221 +1,187 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable]
 public class AnchorPoints
 {
-    public Transform upperPoint;
-    public Transform rightPoint;
-    public Transform lowerPoint;
-    public Transform leftPoint;
+    public Transform topRightPoint;
+    public Transform buttonLeftPoint;
 }
 
 public class CameraController : MonoBehaviour
 {
-    // Transfroms of the two players, assign these in the Inspector
-    public Transform player1Transform;
-    public Transform player2Transform;
+    // Game Object of the two players, assign these in the Inspector
+    public GameObject player1;
+    public GameObject player2;
 
-    // Transform of the background, assign this in the Inspector
-    // this is used to restrict the location of the camera
-
-    // anchors to confine the camera;
+    // Anchors to confine the camera , assign these in the Inspector
     public AnchorPoints anchorPoints;
 
-
-    // Interface: Control the zoom ratio of the Camera, the larger the value, the boarder the view is.
+    // Control the zoom ratio of the Camera, the larger the value, the boarder the view is.
     [Range(1, 3)]
-    private float targetZoomRatio = 1;
+    private float _camZoomRatio = 1;
 
 
     // Default size of the camera, should be set in the Inspector
     // by definition, in orthographic mode, the camera size is half-size of screen's height
-    private const float defaultCamSize = 6.7f;
-    private float customCamSize;
-
-    // Current zoom ratio of the camera
-    private float camZoomRatio = 1.0f;
+    private const float DefaultCamSize = 6.7f;
+    private float _customCamSize;
 
 
 
-    // A flag to change the defaultCamSize, default value is false
-    // If you want to trigger the camera to zoom in/out by your designated size, set it to true to zoom;
-    // it will be set back to false when new size is applied
-    private Camera cam;
-    private Transform camTransform;
+    private Camera _cam;
+    private Transform _camTransform;
+    private float _curCamSize;
 
     // The ratio of the screen, which is used to calculate the camera's width
-    private float aspectRatio;
-
-    private float cameraHeight;
-    private float cameraWidth;
-
-    // Threshold Ratio from x and y axises. if either of them reached, the camera start zooming
-    private float zoomThresholdRatioX = 0.8f;
-    private float zoomThresholdRatioY = 0.8f;
+    private float _aspectRatio;
 
 
-
-    // the x, y bounds of background, to confine the camera movement
-    private float anchorLeftX;
-    private float anchorRightX;
-    private float anchorUpperY;
-    private float anchorLowerY;
+    // Threshold Ratio of the distance to the length of camera's diagnose. if reached, the camera start zooming
+    private const float zoomThresholdRatio = 0.5f;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        camTransform = GetComponent<Transform>();
-        cam = GetComponent<Camera>();
+        _camTransform = GetComponent<Transform>();
+        _cam = GetComponent<Camera>();
 
         // get the aspect ration from Screen
-        aspectRatio = Screen.width * 1.0f / Screen.height;
-        cameraHeight = defaultCamSize * 2;
-        cameraWidth = cameraHeight * aspectRatio;
-        customCamSize = defaultCamSize * camZoomRatio;
+        _aspectRatio = Screen.width * 1.0f / Screen.height;
+        _customCamSize = DefaultCamSize * _camZoomRatio;
 
-        targetZoomRatio = camZoomRatio;
 
         // initialize the camera attributes
-        if (cam)
+        if (_cam)
         {
             // enable the camera
-            cam.enabled = true;
+            _cam.enabled = true;
 
             //enables the orthographic mode and set its initial size
-            cam.orthographic = true;
-            cam.orthographicSize = defaultCamSize;
+            _cam.orthographic = true;
+            _cam.orthographicSize = DefaultCamSize;
 
         }
 
-        // initialize the border of background, for confining camera
-        if (anchorPoints.leftPoint && anchorPoints.rightPoint && anchorPoints.upperPoint && anchorPoints.lowerPoint)
-        {
-            anchorLeftX = anchorPoints.leftPoint.position.x;
-            anchorRightX = anchorPoints.rightPoint.position.x;
-            anchorUpperY = anchorPoints.upperPoint.position.x;
-            anchorLowerY = anchorPoints.lowerPoint.position.x;
-        }
     }
 
     private void Update()
     {
-        // Debug.Log ("player1Transform.position.x = " + player1Transform.position.x);
-        camZoomRatio = Mathf.Lerp(camZoomRatio, targetZoomRatio, 2 * Time.deltaTime);
-        customCamSize = defaultCamSize * camZoomRatio;
+        _customCamSize = Mathf.Lerp(_customCamSize,  DefaultCamSize * _camZoomRatio, 2 * Time.deltaTime);
+        _cam.orthographicSize = Mathf.Lerp(_cam.orthographicSize, _curCamSize, 1.7f * Time.deltaTime);
     }
 
     void LateUpdate()
     {
 
-        if (cam && player1Transform && player2Transform)
+        if (_cam && player1 && player2)
         {
-
-            // 1. update the camera size;
             // get current size of the camera
-            float curCamSize = cam.orthographicSize;
-            cameraHeight = curCamSize * 2;
-            cameraWidth = cameraHeight * aspectRatio;
+            float curCamSize = _cam.orthographicSize;
+            float cameraHeight = curCamSize * 2;
+            float cameraWidth = cameraHeight * _aspectRatio;
             float camDiagnose = (float)Math.Sqrt(cameraHeight * cameraHeight + cameraWidth * cameraWidth);
             float ratioHeight2Diagnose = cameraHeight / camDiagnose;
 
-            // get camera and two players's positions
-            Vector3 camPos = camTransform.position;
-            Vector3 playerPos1 = player1Transform.position;
-            Vector3 playerPos2 = player2Transform.position;
-
-            // calculate the ratio of x-distance and y-distance
-            // if the ratio of the distance between two players takes zoomThresholdRatio, start zooming
-            float nextCamSize = customCamSize;
-            float xDistance = Math.Abs(playerPos1.x - playerPos2.x);
-            float yDistance = Math.Abs(playerPos1.y - playerPos2.y);
-            float xyDistance = (playerPos1 - playerPos2).magnitude;
-
-
-            float distanceRatioX = xDistance / cameraWidth;
-            float distanceRatioY = yDistance / cameraHeight;
-            float distanceRatioXY = xyDistance / camDiagnose;
-            if (Math.Abs(distanceRatioX - zoomThresholdRatioX) < 0.1f)
-            {
-                float nextCamWidth = xDistance / zoomThresholdRatioX;
-                float nextCamHeight = nextCamWidth / aspectRatio;
-                nextCamSize = nextCamHeight / 2;
-                // Debug.Log ("distanceRatioX,  zoomThresholdRatioX = " + distanceRatioX + ", " + zoomThresholdRatioX);
-
-            }
-
-            if (Math.Abs(distanceRatioY - zoomThresholdRatioY) < 0.1f)
-            {
-                float nextCamHeight = yDistance / zoomThresholdRatioY;
-                nextCamSize = nextCamHeight / 2;
-                // Debug.Log ("distanceRatioY,  zoomThresholdRatioY = " + distanceRatioY + ", " + zoomThresholdRatioY);
-            }
-
-            if (Math.Abs(distanceRatioXY - zoomThresholdRatioX) < 0.1f)
-            {
-                float nextCamDiagnose = xyDistance / zoomThresholdRatioX;
-                nextCamSize = nextCamDiagnose * ratioHeight2Diagnose / 2;
-                // Debug.Log ("distanceRatioY,  zoomThresholdRatioY = " + distanceRatioY + ", " + zoomThresholdRatioY);
-            }
-
-            // camera size could not be smaller than the default size;
-            if (nextCamSize < customCamSize)
-            {
-                // Debug.Log ("Reverse back to default size");
-                nextCamSize = customCamSize;
-            }
-
-            // cam.orthographicSize = nextCamSize * camZoomRatio;
-            cam.orthographicSize = nextCamSize;
+            // 1. update the camera size;
+            UpdateCameraSize(camDiagnose, ratioHeight2Diagnose);
 
             // 2. update the position of camera, (within background if assigned)
-            float mid_x = (playerPos1.x + playerPos2.x) / 2;
-            float mid_y = (playerPos1.y + playerPos2.y) / 2;
+            UpdateCameraPosition(cameraWidth, cameraHeight);
 
-
-            // camera position could not leave the following bounded area
-            if (anchorPoints.leftPoint && anchorPoints.rightPoint && anchorPoints.upperPoint && anchorPoints.lowerPoint)
-
-            {
-                if (mid_x < anchorLeftX + cameraWidth / 2)
-                {
-                    mid_x = anchorLeftX + cameraWidth / 2;
-                }
-
-                if (mid_x > anchorRightX - cameraWidth / 2)
-                {
-                    mid_x = anchorRightX - cameraWidth / 2;
-
-                }
-
-                // for future use, if y-axis is needed
-
-                // if (mid_y > bgUpperY - cameraHeight / 2)
-                // {
-                //     mid_y = bgUpperY - cameraHeight / 2;
-                // }
-                //
-                // if (mid_y < bgLowerY + cameraHeight / 2)
-                // {
-                //     mid_y = bgLowerY + cameraHeight / 2;
-                // }
-
-            }
-            camTransform.position = new Vector3(mid_x, mid_y, camPos.z);
         }
     }
 
+
+
     /**
+     * Update the camera size based on the distance of two players
+     *
+     * @Param camDiagnose: length of diagnose of the camera
+     * @Param ratioHeight2Diagnose: the ratio of the height to diagnose of the camera
+     */
+    private void UpdateCameraSize(float camDiagnose, float ratioHeight2Diagnose)
+    {
+        float nextCamSize = _customCamSize;
+        float xyDistance = (player1.transform.position - player2.transform.position).magnitude;
+        float distanceRatioXY = xyDistance / camDiagnose;
+
+        // if the ratio of the distance between two players takes zoomThresholdRatio, start zooming
+        if (Math.Abs(distanceRatioXY - zoomThresholdRatio) < 0.1f)
+        {
+            float nextCamDiagnose = xyDistance / zoomThresholdRatio;
+            nextCamSize = nextCamDiagnose * ratioHeight2Diagnose / 2;
+            // Debug.Log ("distanceRatioY,  zoomThresholdRatioY = " + distanceRatioY + ", " + zoomThresholdRatioY);
+        }
+
+        // camera size could not be smaller than the default size;
+        if (nextCamSize < _customCamSize)
+        {
+            // Debug.Log ("Reverse back to default size");
+            nextCamSize = _customCamSize;
+        }
+
+        // cam.orthographicSize = nextCamSize * camZoomRatio;
+        _curCamSize = nextCamSize;
+    }
+
+    /**
+     * Update the camera position based on the relative position of two players, confine it in the background, if applied
+     *
+     * @Param cameraWidth: width of the camera
+     * @Param cameraHeight: height of the camera
+     */
+    private void UpdateCameraPosition(float cameraWidth, float cameraHeight)
+    {
+        var playerPos1 = player1.transform.position;
+        var playerPos2 = player2.transform.position;
+        float midX = (playerPos1.x + playerPos2.x) / 2;
+        float midY = (playerPos1.y + playerPos2.y) / 2;
+
+
+        // camera position could not leave the following bounded area, if applied
+        if (anchorPoints.topRightPoint && anchorPoints.buttonLeftPoint)
+
+        {
+            if (midX < anchorPoints.buttonLeftPoint.position.x + cameraWidth / 2)
+            {
+                midX = anchorPoints.buttonLeftPoint.position.x + cameraWidth / 2;
+            }
+
+            if (midX > anchorPoints.topRightPoint.position.x - cameraWidth / 2)
+            {
+                midX = anchorPoints.topRightPoint.position.x - cameraWidth / 2;
+
+            }
+
+            // for future use, if y-axis is needed
+
+            if (midY > anchorPoints.topRightPoint.position.y - cameraHeight / 2)
+            {
+                midY = anchorPoints.topRightPoint.position.y - cameraHeight / 2;
+            }
+
+            if (midY < anchorPoints.buttonLeftPoint.position.y + cameraHeight / 2)
+            {
+                midY = anchorPoints.buttonLeftPoint.position.y + cameraHeight / 2;
+            }
+
+        }
+
+        var curCamPos = _camTransform.position;
+        _camTransform.position = Vector3.Lerp(curCamPos, new Vector3(midX, midY, curCamPos.z), 0.01f);
+    }
+
+    /**
+     * public interface for manually set the zooming ratio of the camera
+     *
      * @param: zoomRatio, recommended to be set between 1 to 2.5
      */
     public void setZoomRatio(float zoomRatio)
     {
-        targetZoomRatio = zoomRatio;
+        _camZoomRatio = zoomRatio;
         // Debug.Log ("Set Zoom Ratio");
-
     }
 
 
