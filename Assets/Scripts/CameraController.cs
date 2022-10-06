b69959bfb68c11e7ca86e3951d4563aafd2184cd
XJDKC <xjdkcsq3@gmail.com>
2022-10-06 16:51:38 -0700

    use SmoothDamp to smooth camera zoom and movement

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,6 +8,7 @@ public class AnchorPoints
     public Transform buttonLeftPoint;
 }
 
+[RequireComponent(typeof(Camera))]
 public class CameraController : MonoBehaviour
 {
     // Game Object of the two players, assign these in the Inspector
@@ -17,141 +18,113 @@ public class CameraController : MonoBehaviour
     // Anchors to confine the camera , assign these in the Inspector
     public AnchorPoints anchorPoints;
 
-    // Control the zoom ratio of the Camera, the larger the value, the boarder the view is.
-    [Range(1, 3)]
-    private float _camZoomRatio = 1;
-
-
     // Default size of the camera, should be set in the Inspector
-    // by definition, in orthographic mode, the camera size is half-size of screen's height
-    private const float DefaultCamSize = 6.7f;
-    private float _customCamSize;
+    // By definition, in orthographic mode, the camera size is half-size of screen's height
+    public float defaultCameraSize = 6.7f;
 
-    private Camera _cam;
-    private Transform _camTransform;
-    private float _curCamSize;
-    private Vector3 _curCamPos;
-
-    // The ratio of the screen, which is used to calculate the camera's width
-    private float _aspectRatio;
-
-    // Zoom ratio by two players, ensure the distance of two players to the length of the screen is not smaller than ZoomRatioByPlayers
-    private const float ZoomRatioByPlayers = 0.5f;
+    // The smoothing time used when changing camera position
+    public float moveSmoothTime = 0.25f;
 
     // Factor to control the smoothness when camera size is changing
-    private const float ZoomSmoothFactor = 2.0f;
+    public float zoomSmoothTime = 0.5f;
 
-    private Vector3 _velocity = Vector3.zero;
+    // Zoom ratio by two players, ensure the distance of two players to the length of the screen is not greater than ZoomRatioByPlayers
+    public float zoomRatioByPlayers = 0.5f;
+
+
+    // Related Components
+    private Camera _camera;
+    private Transform _cameraTransform;
+
+    // Control the zoom ratio of the Camera, the larger the value, the broader the view is.
+    private float _cameraZoomRatio = 1;
+    private float _zoomVelocity = 0.0f;
+    private Vector3 _moveVelocity = Vector3.zero;
+
+    private void Awake()
+    {
+        _camera = GetComponent<Camera>();
+        _cameraTransform = GetComponent<Transform>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        _camTransform = GetComponent<Transform>();
-        _cam = GetComponent<Camera>();
-
-        // get the aspect ration from Screen
-        _aspectRatio = Screen.width * 1.0f / Screen.height;
-        _customCamSize = DefaultCamSize * _camZoomRatio;
-
-
         // initialize the camera attributes
-        if (_cam)
+        if (_camera)
         {
             // enable the camera
-            _cam.enabled = true;
+            _camera.enabled = true;
 
-            //enables the orthographic mode and set its initial size
-            _cam.orthographic = true;
-            _cam.orthographicSize = DefaultCamSize;
+            // enables the orthographic mode and set its initial size
+            _camera.orthographic = true;
+            _camera.orthographicSize = defaultCameraSize;
         }
     }
 
-    private void Update()
+    void FixedUpdate()
     {
-        _customCamSize = DefaultCamSize * _camZoomRatio;
-    }
-
-    void LateUpdate()
-    {
-        if (_cam && player1 && player2)
+        if (_camera && player1 && player2)
         {
-            // get current size of the camera
-            float curCamSize = _cam.orthographicSize;
-            float cameraHeight = curCamSize * 2;
-            float cameraWidth = cameraHeight * _aspectRatio;
-            float camDiagnose = (float)Math.Sqrt(cameraHeight * cameraHeight + cameraWidth * cameraWidth);
-            float ratioHeight2Diagnose = cameraHeight / camDiagnose;
-
             // 1. update the camera size;
-            UpdateCameraSize(ratioHeight2Diagnose);
+            UpdateCameraSize();
 
             // 2. update the position of camera, (within background if assigned)
-            UpdateCameraPosition(cameraWidth, cameraHeight);
-
+            UpdateCameraPosition();
         }
     }
 
     /**
      * Update the camera size based on the distance of two players
-     *
-     * @Param ratioHeight2Diagnose: the ratio of the height to diagnose of the camera
      */
-    private void UpdateCameraSize(float ratioHeight2Diagnose)
+    private void UpdateCameraSize()
     {
+        float cameraHeight = _camera.orthographicSize * 2;
+        float cameraWidth = cameraHeight * _camera.aspect;
+        float cameraDiagonal = Mathf.Sqrt(cameraHeight * cameraHeight + cameraWidth * cameraWidth);
         float playerDistance = (player1.transform.position - player2.transform.position).magnitude;
 
         // zoom the camera by the distance of two players
-        float nextCamDiagnose = playerDistance / ZoomRatioByPlayers;
-        float nextCamSize = nextCamDiagnose * ratioHeight2Diagnose / 2;
+        float nextCameraDiagonal = playerDistance / zoomRatioByPlayers;
+        float nextCameraSize = (nextCameraDiagonal / cameraDiagonal) * cameraHeight / 2;
+
+        float minimumCameraSize = defaultCameraSize * _cameraZoomRatio;
 
         // camera size could not be smaller than the default size;
-        if (nextCamSize < _customCamSize)
+        if (nextCameraSize < minimumCameraSize)
         {
-            // Debug.Log ("Reverse back to default size");
-            nextCamSize = _customCamSize;
+            nextCameraSize = minimumCameraSize;
         }
 
-        // cam.orthographicSize = nextCamSize * camZoomRatio;
-        _curCamSize = nextCamSize;
-
-        _cam.orthographicSize = Mathf.Lerp(_cam.orthographicSize, _curCamSize, ZoomSmoothFactor * Time.deltaTime);
+        _camera.orthographicSize =
+            Mathf.SmoothDamp(_camera.orthographicSize, nextCameraSize, ref _zoomVelocity, zoomSmoothTime);
     }
 
     /**
      * Update the camera position based on the relative position of two players, confine it in the background, if applied
-     *
-     * @Param cameraWidth: width of the camera
-     * @Param cameraHeight: height of the camera
      */
-    private void UpdateCameraPosition(float cameraWidth, float cameraHeight)
+    private void UpdateCameraPosition()
     {
         var playerPos1 = player1.transform.position;
         var playerPos2 = player2.transform.position;
         float midX = (playerPos1.x + playerPos2.x) / 2;
         float midY = (playerPos1.y + playerPos2.y) / 2;
 
-
         // camera position could not leave the bounded area, if applied by two anchor points
         if (anchorPoints.topRightPoint && anchorPoints.buttonLeftPoint)
-
         {
-            // Debug.Log ("Camera Confined");
+            float cameraHeight = _camera.orthographicSize * 2;
+            float cameraWidth = cameraHeight * _camera.aspect;
             var buttonLeftPos = anchorPoints.buttonLeftPoint.position;
             var topRightPos = anchorPoints.topRightPoint.position;
             midX = Mathf.Clamp(midX, buttonLeftPos.x + cameraWidth / 2, topRightPos.x - cameraWidth / 2);
-
-            // for future use, if y-axis is needed
             midY = Mathf.Clamp(midY, buttonLeftPos.y + cameraHeight / 2, topRightPos.y - cameraHeight / 2);
-
         }
 
-        var curCamPos = _camTransform.position;
-        var newCamPos = new Vector3(midX, midY, curCamPos.z);
-        // _camTransform.position = Vector3.Lerp(curCamPos, new Vector3(midX, midY, curCamPos.z), 2 * Time.deltaTime);
-        // _camTransform.position = newCamPos;
-
-        // _camTransform.position = Vector3.SmoothDamp(_camTransform.position, newCamPos, ref _velocity, 0.1f);
-        _camTransform.position = Vector3.Lerp(_camTransform.position, newCamPos, 10f);
+        var currCameraPos = _cameraTransform.position;
+        var nextCameraPos = new Vector3(midX, midY, currCameraPos.z);
+        _cameraTransform.position =
+            Vector3.SmoothDamp(currCameraPos, nextCameraPos, ref _moveVelocity, moveSmoothTime);
     }
 
     /**
@@ -161,9 +134,6 @@ public class CameraController : MonoBehaviour
      */
     public void SetZoomRatio(float zoomRatio)
     {
-        _camZoomRatio = zoomRatio;
-        // Debug.Log ("Set Zoom Ratio");
+        _cameraZoomRatio = zoomRatio;
     }
-
-
 }
