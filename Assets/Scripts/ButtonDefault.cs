ff151df81d3ea030c7230fc679672165c06a6afb
simengxu <simengxu@usc.edu>
2022-10-13 16:37:30 -0700

    Added the button design, added change player jump force

---
+++ Assets/Scripts/ButtonDefault.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonDefault : MonoBehaviour
+{
+    //private BoxCollider2D button = GetComponents<BoxCollider2D>();
+    private bool isOn;
+    private GameObject target;
+    private void OnTriggerStay2D(Collider2D other) {
+        if(other.CompareTag("Player")){
+            Debug.Log("Player is on");
+            //EnemyController target =  GameObject.Find("Player2").GetComponent<EnemyController>();
+            // target.enemyReverse = true;
+            foreach (var player in GameObject.FindGameObjectsWithTag("Player"))
+        {
+            var playerType = player.GetComponent<PlayerController>().playerType;
+            var playerSelfType = other.GetComponent<PlayerController>().playerType;
+            Debug.Log("curr playe is " + playerSelfType);
+            Debug.Log("Other player is" + playerType);
+            if (playerType!= playerSelfType){
+                player.GetComponent<PlayerController>().jumpSpeed = 12.0f;
+            }
+            // if (playerType == PlayerController.PlayerType.Player1)
+            // {
+            //     foreach (var playerCollider in player.GetComponents<Collider2D>())
+            //     {
+            //         Physics2D.IgnoreCollision(playerCollider, _boxCollider2D);
+            //     }
+            // }
+        }
+        }
+    }
+}
