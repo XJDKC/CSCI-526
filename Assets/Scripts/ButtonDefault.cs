b44e22e350c01840bba81b64bc301310117ece70
simengxu <xusimeng1999jas@gmail.com>
2022-10-14 22:03:05 -0700

    Fixed the button for changing size of the player, when player leave the button the other player would be back to same size

--- Assets/Scripts/ButtonDefault.cs
+++ Assets/Scripts/ButtonDefault.cs
@@ -5,32 +5,51 @@ using UnityEngine.SceneManagement;
 
 public class ButtonDefault : MonoBehaviour
 {
-    //private BoxCollider2D button = GetComponents<BoxCollider2D>();
     private bool isOn;
     private GameObject target;
-    private void OnTriggerStay2D(Collider2D other) {
+    private void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Player")){
             Debug.Log("Player is on");
-            //EnemyController target =  GameObject.Find("Player2").GetComponent<EnemyController>();
-            // target.enemyReverse = true;
-            foreach (var player in GameObject.FindGameObjectsWithTag("Player"))
+            getSmall(other);
+            isOn = true;
+        }
+        
+    }
+    private void getSmall(Collider2D other){
+        foreach (var player in GameObject.FindGameObjectsWithTag("Player"))
         {
             var playerType = player.GetComponent<PlayerController>().playerType;
             var playerSelfType = other.GetComponent<PlayerController>().playerType;
-            Debug.Log("curr playe is " + playerSelfType);
-            Debug.Log("Other player is" + playerType);
             if (playerType!= playerSelfType){
-                //player.GetComponent<PlayerController>().jumpSpeed = 12.0f;
                 player.transform.localScale = new Vector3(0.5f,0.5f,1);
+                return;               
                 }
-            // if (playerType == PlayerController.PlayerType.Player1)
-            // {
-            //     foreach (var playerCollider in player.GetComponents<Collider2D>())
-            //     {
-            //         Physics2D.IgnoreCollision(playerCollider, _boxCollider2D);
-            //     }
-            // }
         }
+        return;
+    }
+
+    private void getLarge(Collider2D other){
+        foreach (var player in GameObject.FindGameObjectsWithTag("Player"))
+        {
+            var playerType = player.GetComponent<PlayerController>().playerType;
+            var playerSelfType = other.GetComponent<PlayerController>().playerType;
+            if (playerType!= playerSelfType){
+                player.transform.localScale = new Vector3(1,1,1);
+                return;               
+                }
+        }
+        return;
+    }
+
+    private void OnTriggerExit2D(Collider2D other){
+        if(other.CompareTag("Player")){
+            getLarge(other);
+            isOn = false;
         }
     }
+    void Update(){
+        Debug.Log("current state is " + isOn);
+
+    }
+
 }
