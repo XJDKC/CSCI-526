de8f01ab9eb76aab85d866c810096aa6c7556684
JohnsonL-G8 <liusiyug8@gmail.com>
2022-10-18 16:08:01 -0700

    delete data_post update to data_collect

--- Assets/Scripts/Star.cs
+++ Assets/Scripts/Star.cs
@@ -47,8 +47,9 @@ public class Star : MonoBehaviour, IReversible
         if (collider.gameObject.CompareTag("Player") &&
             collider.GetType().ToString() == "UnityEngine.CapsuleCollider2D")
         {
-            StarUI.CurrentStarQuantity += 1;
-            Debug.Log(StarUI.CurrentStarQuantity);
+            //data collect
+            DataManager.Instance.AddStarPoints();
+
             Destroy(gameObject);
         }
     }
