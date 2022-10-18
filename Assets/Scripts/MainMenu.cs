62e5cfdbb35214134065b86c53bd04b30d40b68c
yaqiuxu <carina_xuyq@163.com>
2022-10-17 22:07:44 -0700

    updated main menu and file names

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,36 +7,41 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayLevel1()
     {
-        SceneManager.LoadScene(6);
+        SceneManager.LoadScene(5);
     }
 
-    public void PlayTutorialLevel0()
+    public void PlayTutorialGravityPortal()
     {
         SceneManager.LoadScene(1);
     }
 
-    public void PlayTutorialLevel1()
+    public void PlayTutorialKillMonsters()
     {
         SceneManager.LoadScene(2);
     }
 
-    public void PlayTutorialLevel2()
+    public void PlayTutorialDoubleJump()
     {
         SceneManager.LoadScene(3);
     }
 
-    public void PlayTutorialLevel3()
+    public void PlayTutorialHighPlatform()
     {
         SceneManager.LoadScene(4);
     }
 
-    public void PlayTutorialLevel4()
-    {
-        SceneManager.LoadScene(5);
-    }
-    
-    public void PlayTutorialLevel5()
+    public void PlayTutorialButtons()
     {
         SceneManager.LoadScene(6);
+    }
+
+    public void PlayTutorialGravityCookie()
+    {
+        SceneManager.LoadScene(7);
+    }
+
+    public void PlayLevel2()
+    {
+        SceneManager.LoadScene(8);
     }
 }
