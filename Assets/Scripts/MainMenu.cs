e551fef3d7ad0b51b9b79d49cc359495597d627d
icecoffee98 <yixuanyu98@gmail.com>
2022-10-18 18:25:01 -0700

    main menu detached

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,46 +7,46 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayLevel1()
     {
-        SceneManager.LoadScene(5);
+        // SceneManager.LoadScene(5);
     }
 
     public void PlayTutorialGravityPortal()
     {
-        SceneManager.LoadScene(1);
+        // SceneManager.LoadScene(1);
     }
 
     public void PlayTutorialKillMonsters()
     {
-        SceneManager.LoadScene(2);
+        // SceneManager.LoadScene(2);
     }
 
     public void PlayTutorialDoubleJump()
     {
-        SceneManager.LoadScene(3);
+        // SceneManager.LoadScene(3);
     }
 
     public void PlayTutorialHighPlatform()
     {
-        SceneManager.LoadScene(4);
+        // SceneManager.LoadScene(4);
     }
 
     public void PlayTutorialButtons()
     {
-        SceneManager.LoadScene(6);
+        // SceneManager.LoadScene(6);
     }
 
     public void PlayTutorialGravityCookie()
     {
-        SceneManager.LoadScene(7);
+        // SceneManager.LoadScene(7);
     }
 
     public void PlayLevel2()
     {
-        SceneManager.LoadScene(8);
+        // SceneManager.LoadScene(8);
     }
 
     public void PlayTutorialBounceBelt()
     {
-        SceneManager.LoadScene(9);
+        // SceneManager.LoadScene(9);
     }
 }
