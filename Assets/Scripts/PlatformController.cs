9caa223f0fb50183a883fcc11ed285c9f10d586d
msyysm1211 <mashengyuan1997@gmail.com>
2022-10-20 14:54:45 -0700

    revise platform shake

--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 
 public class PlatformController : MonoBehaviour
 {
-    public PlatformType type = PlatformType.HorizontalEnemy;
+    public PlatformType type = PlatformType.HorizontalPlatform;
     public float moveDistance = 3f;
     public float speed = 3f;
     public bool isReverse;
-    private Transform _playerTransform1=null;
-    private Transform _playerTransform2=null;
+    private Transform _playerTransform1 = null;
+    private Transform _playerTransform2 = null;
 
     public enum PlatformType
     {
-        HorizontalEnemy = 1,
-        VerticalEnemy = 2
+        HorizontalPlatform = 1,
+        VerticalPlatform = 2
     }
 
     private float _initialPositionX = 0;
@@ -43,18 +43,18 @@ public class PlatformController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (type == PlatformType.HorizontalEnemy)
+        if (type == PlatformType.HorizontalPlatform)
         {
             HorizontalMove();
         }
 
-        if (type == PlatformType.VerticalEnemy)
+        if (type == PlatformType.VerticalPlatform)
         {
             VerticalMove();
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D col)
+    private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.CompareTag("Player"))
         {
@@ -62,15 +62,7 @@ public class PlatformController : MonoBehaviour
         }
     }
 
-    private void OnCollisionStay2D(Collision2D col)
-    {
-        if (col.gameObject.CompareTag("Player"))
-        {
-            col.transform.parent = gameObject.transform;
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D other)
+    private void OnTriggerExit2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
@@ -85,6 +77,7 @@ public class PlatformController : MonoBehaviour
             }
         }
     }
+
 
     void HorizontalMove()
     {
