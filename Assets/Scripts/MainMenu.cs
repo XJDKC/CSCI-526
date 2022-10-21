5d7e031ba80b1b2e8a1ec2e08e6d251057585fae
yaqiuxu <carina_xuyq@163.com>
2022-10-21 00:33:08 -0700

    add data hook in MainMenu script

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,95 +8,133 @@ public class MainMenu : MonoBehaviour
     public void PlayLevel1_1()
     {
         SceneManager.LoadScene("Level1-1");
+        DataManager.levelName = "Level1-1";
+        DataManager.GetStartTime();
     }
 
     public void PlayLevel1_2()
     {
         SceneManager.LoadScene("Level1-2");
+        DataManager.levelName = "Level1-2";
+        DataManager.GetStartTime();
     }
 
     public void PlayLevel1_3()
     {
         SceneManager.LoadScene("Level1-3");
+        DataManager.levelName = "Level1-3";
+        DataManager.GetStartTime();
     }
 
     public void PlayLevel1_4()
     {
         SceneManager.LoadScene("Level1-4");
+        DataManager.levelName = "Level1-4";
+        DataManager.GetStartTime();
     }
 
     public void PlayLevel2_1()
     {
         SceneManager.LoadScene("Level2-1");
+        DataManager.levelName = "Level2-1";
+        DataManager.GetStartTime();
     }
 
     public void PlayLevel2_2()
     {
         SceneManager.LoadScene("Level2-2");
+        DataManager.levelName = "Level2-2";
+        DataManager.GetStartTime();
     }
 
     public void PlayLevel2_3()
     {
         SceneManager.LoadScene("Level2-3");
+        DataManager.levelName = "Level2-3";
+        DataManager.GetStartTime();
     }
 
     public void PlayLevel3_1()
     {
         SceneManager.LoadScene("Level3-1");
+        DataManager.levelName = "Level3-1";
+        DataManager.GetStartTime();
     }
 
     public void PlayLevel3_2()
     {
         SceneManager.LoadScene("Level3-2");
+        DataManager.levelName = "Level3-2";
+        DataManager.GetStartTime();
     }
 
     public void PlayLevel3_3()
     {
         SceneManager.LoadScene("Level3-3");
+        DataManager.levelName = "Level3-3";
+        DataManager.GetStartTime();
     }
 
     public void PlayLevel3_4()
     {
         SceneManager.LoadScene("Level3-4");
+        DataManager.levelName = "Level3-4";
+        DataManager.GetStartTime();
     }
 
     public void PlayLevel4_1()
     {
         SceneManager.LoadScene("Level4-1");
+        DataManager.levelName = "Level4-1";
+        DataManager.GetStartTime();
     }
 
     public void PlayLevel4_2()
     {
         SceneManager.LoadScene("Level4-2");
+        DataManager.levelName = "Level4-2";
+        DataManager.GetStartTime();
     }
 
     public void PlayLevel4_3()
     {
         SceneManager.LoadScene("Level4-3");
+        DataManager.levelName = "Level4-3";
+        DataManager.GetStartTime();
     }
 
     public void PlayLevel5_1()
     {
         SceneManager.LoadScene("Level5-1");
+        DataManager.levelName = "Level5-1";
+        DataManager.GetStartTime();
     }
 
     public void PlayLevel5_2()
     {
         SceneManager.LoadScene("Level5-2");
+        DataManager.levelName = "Level5-2";
+        DataManager.GetStartTime();
     }
 
     public void PlayLevel5_3()
     {
         SceneManager.LoadScene("Level5-3");
+        DataManager.levelName = "Level5-3";
+        DataManager.GetStartTime();
     }
 
     public void PlayLevel6_1()
     {
         SceneManager.LoadScene("Level6-1");
+        DataManager.levelName = "Level6-1";
+        DataManager.GetStartTime();
     }
 
     public void PlayLevel6_2()
     {
         SceneManager.LoadScene("Level6-2");
+        DataManager.levelName = "Level6-2";
+        DataManager.GetStartTime();
     }
 }
