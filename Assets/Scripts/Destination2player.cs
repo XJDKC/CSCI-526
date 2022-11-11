53fb06a5a61285394e62c1c296c2508fd624d35d
XJDKC <xjdkcsq3@gmail.com>
2022-11-10 19:11:32 -0800

    fix the bug of the destination

--- Assets/Scripts/Destination2player.cs
+++ Assets/Scripts/Destination2player.cs
@@ -5,45 +5,80 @@ using UnityEngine.SceneManagement;
 
 public class Destination2player : MonoBehaviour
 {
-    private bool player1Arr=false;
-    private bool player2Arr=false;
-    private void OnTriggerEnter2D(Collider2D other) {
-        if(other.CompareTag("Player")){
-            if(other.GetComponent<PlayerController>().playerType ==PlayerController.PlayerType.Player1){
-                //Debug.Log("player1 reached");
+    private bool player1Arr;
+    private bool player2Arr;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (other.GetComponent<PlayerController>().playerType == PlayerController.PlayerType.Player1)
+            {
+                // Debug.Log("Player1 reached");
                 player1Arr = true;
-            }else{
-                //Debug.Log("Player2 reached");
+            }
+            else
+            {
+                // Debug.Log("Player2 reached");
                 player2Arr = true;
             }
         }
     }
 
-    private void OnTriggerExit2D(Collider2D other) {
-        if(other.CompareTag("Player")){
-            if(other.GetComponent<PlayerController>().playerType ==PlayerController.PlayerType.Player1){
-                //Debug.Log("player1 reached");
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (other.GetComponent<PlayerController>().playerType == PlayerController.PlayerType.Player1)
+            {
+                // Debug.Log("Player1 stayed");
+                player1Arr = true;
+            }
+            else
+            {
+                // Debug.Log("Player2 stayed");
+                player2Arr = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (other.GetComponent<PlayerController>().playerType == PlayerController.PlayerType.Player1)
+            {
+                // Debug.Log("Player1 leaved");
                 player1Arr = false;
-            }else{
-                //Debug.Log("Player2 reached");
+            }
+            else
+            {
+                //Debug.Log("Player2 leaved");
                 player2Arr = false;
             }
         }
     }
+
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(player1Arr&&player2Arr){
-            if (!GameObject.Find("ScoreTMP").GetComponent<StarUI>().getStatus())
+        if (player1Arr && player2Arr)
+        {
+            var starUI = GameObject.FindObjectOfType<StarUI>();
+            if (starUI && !starUI.getStatus())
             {
-                GameObject.Find("PanelPrompt").GetComponent<Guide>().mininumScorePanel();
-                GameObject.Find("TextPrompt").GetComponent<GuideMessage>().minimumScoreText();
+                var guide = GameObject.FindObjectOfType<Guide>();
+                var guideMessage = GameObject.FindObjectOfType<GuideMessage>();
+                if (guide && guideMessage)
+                {
+                    guide.mininumScorePanel();
+                    guideMessage.minimumScoreText();
+                }
             }
             else
             {
