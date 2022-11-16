86c4c7fbd86fcc20e9731c2796b35b730eb2b01b
yaqiuxu <carina_xuyq@163.com>
2022-11-16 10:00:32 -0800

    add button animations for lvl4-2

--- Assets/Scripts/ButtonChangeGravityScale.cs
+++ Assets/Scripts/ButtonChangeGravityScale.cs
@@ -16,41 +16,48 @@ public class ButtonChangeGravityScale : MonoBehaviour
     public GameObject plus;
     public GameObject minus;
 
-    private Vector3 position;
+    private Vector3 _position;
+    private Animator _myAnimator;
     private void Start()
     {
-        position = transform.position;
+        _position = transform.position;
+        _myAnimator = GetComponent<Animator>();
         if (buttonType == ButtonType.Plus)
         {
-            GetComponent<SpriteRenderer>().color = new Color(240, 90, 90);
+            // GetComponent<SpriteRenderer>().color = new Color(240, 90, 90);
             plus.SetActive(true);
-            plus.transform.position = new Vector3(position.x - 0.6f, position.y, position.z);
+            plus.transform.position = new Vector3(_position.x - 0.6f, _position.y, _position.z);
         }
         else
         {
             minus.SetActive(true);
-            minus.transform.position = new Vector3(position.x -0.6f, position.y, position.z);
+            minus.transform.position = new Vector3(_position.x -0.6f, _position.y, _position.z);
         }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        _myAnimator.SetBool("down", true);
+
         foreach (var player in GameObject.FindGameObjectsWithTag("Player"))
         {
             var thisPlayer = other;
             var otherPlayer = player;
+            bool touchongGround;
 
             if (thisPlayer.GetComponent<PlayerController>().playerType != otherPlayer.GetComponent<PlayerController>().playerType)
             {
                 var currGravityScale = otherPlayer.GetComponent<Rigidbody2D>().gravityScale;
+                touchongGround = otherPlayer.GetComponent<BoxCollider2D>()
+                    .IsTouchingLayers(LayerMask.GetMask("Ground"));
 
-                if (buttonType == ButtonType.Minus)
+                if (buttonType == ButtonType.Minus && touchongGround)
                 {
                     currGravityScale = (currGravityScale > 0 ?
                         Math.Max(lowerBound, currGravityScale - minusDelta) : Math.Min(-lowerBound, currGravityScale + minusDelta));
                 }
 
-                else if (buttonType == ButtonType.Plus)
+                else if (buttonType == ButtonType.Plus && touchongGround)
                 {
                     currGravityScale = (currGravityScale > 0 ?
                         Math.Min(upperBound, currGravityScale + plusDelta) : Math.Max(-upperBound, currGravityScale - plusDelta));
@@ -58,7 +65,7 @@ public class ButtonChangeGravityScale : MonoBehaviour
 
                 otherPlayer.GetComponent<Rigidbody2D>().gravityScale = currGravityScale;
 
-                if(otherPlayer.GetComponent<BoxCollider2D>().IsTouchingLayers(LayerMask.GetMask("Ground")))
+                if(touchongGround)
                 {
                     Vector2 jumpVel = new Vector2(0.0f, otherPlayer.GetComponent<PlayerController>().jumpSpeed);
                     otherPlayer.GetComponent<Rigidbody2D>().velocity = currGravityScale > 0 ? Vector2.up * jumpVel: Vector2.down * jumpVel;
@@ -69,20 +76,10 @@ public class ButtonChangeGravityScale : MonoBehaviour
         }
     }
 
-    /*
+
     private void OnTriggerExit2D(Collider2D other)
     {
-        foreach (var player in GameObject.FindGameObjectsWithTag("Player"))
-        {
-            var thisPlayer = other;
-            var otherPlayer = player;
-            if (thisPlayer.GetComponent<PlayerController>().playerType != otherPlayer.GetComponent<PlayerController>().playerType)
-            {
-                var currGravityScale = otherPlayer.GetComponent<Rigidbody2D>().gravityScale;
-                currGravityScale = currGravityScale > 0 ? 2 : -2;
-                otherPlayer.GetComponent<Rigidbody2D>().gravityScale = currGravityScale;
-            }
-        }
+        _myAnimator.SetBool("down", false);
     }
-    */
+
 }
