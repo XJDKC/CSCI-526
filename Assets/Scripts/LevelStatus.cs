f12b27c064f989dc2bf6c150bffebf05da2984d2
JohnsonL-G8 <liusiyug8@gmail.com>
2022-11-22 15:07:13 -0800

    change data structure for storing the complete levels

--- Assets/Scripts/LevelStatus.cs
+++ Assets/Scripts/LevelStatus.cs
@@ -9,6 +9,7 @@ public class LevelStatus : MonoBehaviour
     public static LevelStatus Instance { get { return _instance; } }
 
     public static Hashtable completeStatus = new Hashtable();
+    public static HashSet<string> completeLevels = new HashSet<string>();
 
     void Awake()
     {
@@ -22,30 +23,6 @@ public class LevelStatus : MonoBehaviour
             _instance = this;
         }
         DontDestroyOnLoad(gameObject);
-        if (completeStatus.Count == 0)
-        {
-            completeStatus.Add("Level1-1", false);
-            completeStatus.Add("Level1-2", false);
-            completeStatus.Add("Level1-3", false);
-            completeStatus.Add("Level1-4", false);
-            completeStatus.Add("Level2-1", false);
-            completeStatus.Add("Level2-2", false);
-            completeStatus.Add("Level2-3", false);
-            completeStatus.Add("Level3-1", false);
-            completeStatus.Add("Level3-2", false);
-            completeStatus.Add("Level3-3", false);
-            completeStatus.Add("Level4-1", false);
-            completeStatus.Add("Level4-2", false);
-            completeStatus.Add("Level4-3", false);
-            completeStatus.Add("Level5-1", false);
-            completeStatus.Add("Level5-2", false);
-            completeStatus.Add("Level5-3", false);
-            completeStatus.Add("Level6-1", false);
-            completeStatus.Add("Level6-2", false);
-            completeStatus.Add("Level7-1", false);
-            completeStatus.Add("Level7-2", false);
-            completeStatus.Add("Level7-3", false);
-        }
     }
 
     public static void ChangeLevelStatus()
@@ -54,10 +31,10 @@ public class LevelStatus : MonoBehaviour
         {
             string level = DataManager.levelName;
             Debug.Log(level);
-            Debug.Log(completeStatus[level]);
-            completeStatus.Remove(level);
-            completeStatus.Add(level, true);
-            Debug.Log(completeStatus[level]);
+            if (!completeLevels.Contains(level))
+            {
+                completeLevels.Add(level);
+            }
         }
         else
         {
