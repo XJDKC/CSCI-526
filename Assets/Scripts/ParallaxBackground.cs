16054c1a9fb0e8b8ea4c0ad0319bf504685d4cd9
yaqiuxu <carina_xuyq@163.com>
2022-11-30 00:57:42 -0800

    add script for parallax background

---
+++ Assets/Scripts/ParallaxBackground.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    public Camera mainCamera;
+
+    [SerializeField] private Vector2 parallaxEffectMultiplier;
+    [SerializeField] private bool infiniteHorizontal;
+    [SerializeField] private bool infiniteVertical;
+
+    private Transform _cameraTransform;
+    private Vector3 _lastCameraPosition;
+    private float _textureUnitSizeX;
+    private float _textureUnitSizeY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cameraTransform = mainCamera.transform;
+        _lastCameraPosition = _cameraTransform.position;
+
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D texture = sprite.texture;
+        _textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
+        _textureUnitSizeY = texture.height / sprite.pixelsPerUnit;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 deltaMovement = _cameraTransform.position - _lastCameraPosition;
+        transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, deltaMovement.y * parallaxEffectMultiplier.y);
+        _lastCameraPosition = _cameraTransform.position;
+
+        if (infiniteHorizontal)
+        {
+            if (Mathf.Abs(_cameraTransform.position.x - transform.position.x) >= _textureUnitSizeX)
+            {
+                float offsetPositionX = (_cameraTransform.position.x - transform.position.x) % _textureUnitSizeX;
+                transform.position = new Vector3(_cameraTransform.position.x + offsetPositionX, transform.position.y);
+            }
+        }
+
+        if (infiniteVertical)
+        {
+            if (Mathf.Abs(_cameraTransform.position.y - transform.position.y) >= _textureUnitSizeY)
+            {
+                float offsetPositionY = (_cameraTransform.position.y - transform.position.y) % _textureUnitSizeY;
+                transform.position = new Vector3(_cameraTransform.position.x, transform.position.y + offsetPositionY);
+            }
+        }
+    }
+}
