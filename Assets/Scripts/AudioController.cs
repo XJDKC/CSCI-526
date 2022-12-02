a565ce9c6dc2cffd72ea831ebab63d344cba0ff1
XJDKC <xjdkcsq3@gmail.com>
2022-12-02 02:26:32 -0800

    fix the issue of playing audio

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -9,24 +9,25 @@ public class Sound
 {
     public string name;
     public AudioClip clip;
-    [Range(0f, 1f)]
-    public float volume = 1f;
-    [Range(0f, 1f)]
-    public float pitch = 0.7f;
+    [Range(0f, 1f)] public float volume = 1f;
+    [Range(0f, 1f)] public float pitch = 0.7f;
 
     private AudioSource _theAs;
 
-    public void SetUpSound(AudioSource theAs) {
+    public void SetUpSound(AudioSource theAs)
+    {
         _theAs = theAs;
         _theAs.clip = clip;
     }
 
-    public void PlayOneShot() {
+    public void PlayOneShot()
+    {
         SetPitchAndVolume();
         _theAs.PlayOneShot(clip, volume);
     }
 
-    public void Play() {
+    public void Play()
+    {
         SetPitchAndVolume();
         _theAs.Play();
     }
@@ -55,10 +56,10 @@ public class AudioController : MonoBehaviour
     private static AudioController _instance;
     public static AudioController Instance { get { return _instance; } }
 
-    [SerializeField]
-    private Sound[] sounds;
+    [SerializeField] private Sound[] sounds;
 
     private readonly Dictionary<string, Sound> _audioMap = new Dictionary<string, Sound>();
+
     private void Awake()
     {
         if (_instance != null && _instance != this)
@@ -68,29 +69,34 @@ public class AudioController : MonoBehaviour
         else
         {
             _instance = this;
-            foreach(var sound in sounds){
+            foreach (var sound in sounds)
+            {
                 _audioMap.Add(sound.name, sound);
                 sound.SetUpSound(gameObject.AddComponent<AudioSource>());
             }
+
             SceneManager.sceneLoaded += OnSceneLoaded;
         }
     }
 
-    public void Play(string _name) {
+    public void Play(string _name)
+    {
         if (_audioMap.ContainsKey(_name))
         {
             _audioMap[_name].Play();
         }
     }
 
-    public void PlayOneShot(string _name) {
+    public void PlayOneShot(string _name)
+    {
         if (_audioMap.ContainsKey(_name))
         {
             _audioMap[_name].PlayOneShot();
         }
     }
 
-    public void PlayBackgroundMusic(string _name) {
+    public void PlayBackgroundMusic(string _name)
+    {
         if (_audioMap.ContainsKey(_name))
         {
             _audioMap[_name].PlayBackgroundMusic();
@@ -100,24 +106,31 @@ public class AudioController : MonoBehaviour
     //stop all bgms and sounds
     private void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
     {
-        foreach(var sound in sounds){
+        foreach (var sound in sounds)
+        {
             sound.Stop();
         }
+
         String sceneName = scene.name;
+        String bgmName = null;
         if (sceneName == "StartScene")
         {
-            PlayBackgroundMusic("Main");
-            return;
+            bgmName = "Main";
         }
-        if (sceneName == "Menu")
+        else if (sceneName == "Menu")
         {
-            PlayBackgroundMusic("Menu");
-            return;
+            bgmName = "Menu";
+        }
+        else
+        {
+            int pos = sceneName.IndexOf("Level");
+            if (pos >= 0)
+            {
+                int level = int.Parse(sceneName.Substring(pos + 5, 1));
+                bgmName = "BGM" + (level % 4 + 1);
+            }
         }
 
-        // now have 4 bgm music, loop to use them
-        int level = int.Parse(sceneName.Substring(sceneName.IndexOf('l') + 1, 1));
-        String bgmName = "BGM" + (level % 4 + 1);
-        PlayBackgroundMusic(bgmName);
+        if (bgmName != null) PlayBackgroundMusic(bgmName);
     }
 }
