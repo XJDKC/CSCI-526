951c0f598c45fb39c5c3c799d79c9caebf279ae9
XJDKC <xjdkcsq3@gmail.com>
2022-12-02 00:38:07 -0800

    update GravitySlides

--- Assets/Scripts/GravitySlide.cs
+++ Assets/Scripts/GravitySlide.cs
@@ -1,14 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GravitySlide : MonoBehaviour
 {
-    public enum SlideMode {Top, Down};
-    public SlideMode slideMode = SlideMode.Top;
+    public enum SlideMode { Up, Down };
+
+    public float scrollSpeed = 0.2f;
+    public SlideMode slideMode = SlideMode.Up;
+
+    private SpriteRenderer _spriteRenderer;
     private Dictionary<Collider2D, bool> _colliderChangeGravity = new Dictionary<Collider2D, bool>();
-    
-    private void OnTriggerStay2D(Collider2D collider) {
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        float rotationZ = slideMode == SlideMode.Up ? 0.0f : 180.0f;
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+    }
+
+    private void Update()
+    {
+        var offsetY = scrollSpeed * Time.time;
+        _spriteRenderer.material.mainTextureOffset = new Vector2(0.0f, -offsetY);
+    }
+
+    private void OnTriggerStay2D(Collider2D collider)
+    {
         var reversibleObject = collider.gameObject.GetComponent<IReversible>();
         if (reversibleObject == null) return;
 
@@ -19,38 +42,41 @@ public class GravitySlide : MonoBehaviour
 
         // check if the box contains player's center
         var bound = GetComponent<BoxCollider2D>().bounds;
-        if (bound.Contains(collider.bounds.center)){
+        if (bound.Contains(collider.bounds.center))
+        {
             playerInBound = true;
         }
 
         // if player's center in the box bounds
-        if (playerInBound == true) {
+        if (playerInBound == true)
+        {
             // already change player's gravity
             if (_colliderChangeGravity.ContainsKey(collider)) return;
 
             var gravityScale = collider.gameObject.GetComponent<Rigidbody2D>().gravityScale;
-            if (gravityScale > 0 && slideMode == SlideMode.Top ||
-                gravityScale < 0 && slideMode == SlideMode.Down) {
-                
+            if (gravityScale > 0 && slideMode == SlideMode.Up ||
+                gravityScale < 0 && slideMode == SlideMode.Down)
+            {
                 reversibleObject.Reverse();
 
                 _colliderChangeGravity[collider] = true;
-                
-            } else {
+            }
+            else
+            {
                 _colliderChangeGravity[collider] = false;
             }
-        } else {
+        }
+        else
+        {
             // already change player's gravity, and player is leaving the box
             if (_colliderChangeGravity.ContainsKey(collider))
             {
                 // if player has changed gravity
-                if (_colliderChangeGravity[collider])   reversibleObject.Reverse();
+                if (_colliderChangeGravity[collider]) reversibleObject.Reverse();
 
                 // clean the dictionary
                 _colliderChangeGravity.Remove(collider);
             }
         }
     }
-
-
 }
