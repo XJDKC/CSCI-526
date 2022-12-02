fd6e326294bc6c9bf7e373a6ca3a04729c88fed5
icecoffee98 <yixuanyu98@gmail.com>
2022-12-01 18:54:25 -0800

    AudioController added

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+    [Range(0f, 1f)]
+    public float volume = 1f;
+    [Range(0f, 1f)]
+    public float pitch = 0.7f;
+
+    private AudioSource theAS;
+
+    public void SetUpSound(AudioSource _theAS) {
+        theAS = _theAS;
+        theAS.clip = clip;
+    }
+
+    public void Play() {
+        theAS.pitch = pitch;
+        theAS.volume = volume;
+        theAS.Play();
+    }
+}
+
+public class AudioController : MonoBehaviour
+{
+    public static AudioController instance;
+
+    [SerializeField]
+    private Sound[] sounds;
+
+    Dictionary<string, Sound> audioMap = new Dictionary<string, Sound>();
+    private void Awake()
+    {
+        if (instance == null) {
+            instance = this;
+            foreach(var sound in sounds){
+                audioMap.Add(sound.name, sound);
+            }
+        }
+        else if (instance != this) {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        foreach (var sound in sounds) {
+            sound.SetUpSound(new GameObject().AddComponent<AudioSource>());
+        }
+    }
+
+    public void Play(string _name) {
+        if (audioMap.ContainsKey(_name))
+        {
+            audioMap[_name].Play();
+        }
+    }
+}
